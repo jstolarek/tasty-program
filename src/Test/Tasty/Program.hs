14dfa96fe1a918d8068d6bf839a66da451439e8f
Jan Stolarek <jan.stolarek@p.lodz.pl>
2014-01-30 19:18:17 +0100

    Code cleanup, some haddock

--- src/Test/Tasty/Program.hs
+++ src/Test/Tasty/Program.hs
@@ -1,46 +1,42 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
-module Test.Tasty.Program where
+module Test.Tasty.Program (
+   testProgram
+ ) where
 
-import Control.Monad (unless)
-import Data.Typeable (Typeable)
-import System.Directory (doesFileExist, getPermissions, executable)
-import System.FilePath (pathSeparator)
-import System.Exit (ExitCode(..))
-import System.IO (hGetContents)
-import System.Process (runInteractiveProcess, waitForProcess)
-import Test.Tasty.Providers (TestName, TestTree, Result(..), IsTest (..), singleTest)
+import Data.Typeable        ( Typeable                              )
+import System.Directory     ( findExecutable                        )
+import System.Exit          ( ExitCode(..)                          )
+import System.Process       ( runInteractiveProcess, waitForProcess )
+import Test.Tasty.Providers ( IsTest (..), Result(..), TestName,
+                              TestTree, singleTest                  )
 
-data TestProgram = TestProgram {
-      testProgramName :: String
-    , testProgramOpts :: [String]
-    , testProgramPath :: Maybe FilePath
-    } deriving (Typeable)
+data TestProgram = TestProgram String [String] (Maybe FilePath)
+     deriving (Typeable)
 
-testProgram :: TestName
-            -> String          -- program to run
-            -> [String]        -- options
-            -> Maybe FilePath  -- working dir
+-- | Create test that will run a program with given options
+testProgram :: TestName        -- ^ Test name
+            -> String          -- ^ Program name
+            -> [String]        -- ^ Program options
+            -> Maybe FilePath  -- ^ Optional working directory
             -> TestTree
 testProgram testName program opts workingDir =
     singleTest testName (TestProgram program opts workingDir)
 
 instance IsTest TestProgram where
   run _ (TestProgram program opts workingDir) _ = do
-    fileExistsFlag <- doesFileExist  program
-    if not fileExistsFlag
-    then return $ fileDoesNotExistFailure program
-    else do
-      execAllowedFlag <- execPermission program
-      if not execAllowedFlag
-      then return $ noExecPermissionFailure program
-      else runProgram program opts workingDir
+    execFound <- findExecutable program
+    case execFound of
+      Nothing       -> return $ execNotFoundFailure program
+      Just progPath -> runProgram progPath opts workingDir
 
   testOptions = return []
 
-runProgram :: String
-           -> [String]        -- options
-           -> Maybe FilePath  -- working dir
+-- | Run a program with given options and optional working directory.
+-- Return success if program exits with success code.
+runProgram :: String          -- ^ Program name
+           -> [String]        -- ^ Program options
+           -> Maybe FilePath  -- ^ Optional working directory
            -> IO Result
 runProgram program opts workingDir = do
   (_, _, _, pid) <- runInteractiveProcess program opts workingDir Nothing
@@ -49,25 +45,18 @@ runProgram program opts workingDir = do
     ExitSuccess      -> return success
     ExitFailure code -> return $ exitFailure program code
 
-execPermission :: FilePath -> IO Bool
-execPermission file = do
-  getPermissions file >>= (return . executable)
-
+-- | Indicates successful test
 success :: Result
 success = Result True ""
 
-fileDoesNotExistFailure :: String -> Result
-fileDoesNotExistFailure file = Result
+-- | Indicates that program does not exist in the path
+execNotFoundFailure :: String -> Result
+execNotFoundFailure file = Result
   { resultSuccessful  = False
-  , resultDescription = "Cannot locate program " ++ file
+  , resultDescription = "Cannot locate program " ++ file ++ " in the PATH"
   }
 
-noExecPermissionFailure :: String -> Result
-noExecPermissionFailure file = Result
-  { resultSuccessful  = False
-  , resultDescription = "No permissions to execute " ++ file
-  }
-
+-- | Indicates that program failed with an error code
 exitFailure :: String -> Int -> Result
 exitFailure file code = Result
   { resultSuccessful  = False
